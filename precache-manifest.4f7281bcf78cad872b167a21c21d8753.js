85af9e587646fdde82793aaeb48cb47fe949cafa
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2019-10-05 18:00:23 -0700

    Updates

---
+++ precache-manifest.4f7281bcf78cad872b167a21c21d8753.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27faf6e43b96735e984abe95826bc570",
+    "url": "/simplenote/index.html"
+  },
+  {
+    "revision": "5a402d9c62d7ea55ee96",
+    "url": "/simplenote/static/css/main.f80e1869.chunk.css"
+  },
+  {
+    "revision": "071222d2a8358d426394",
+    "url": "/simplenote/static/js/2.56edc678.chunk.js"
+  },
+  {
+    "revision": "5a402d9c62d7ea55ee96",
+    "url": "/simplenote/static/js/main.9275cadb.chunk.js"
+  },
+  {
+    "revision": "15a2a3d5632cac234607",
+    "url": "/simplenote/static/js/runtime-main.ae274dfc.js"
+  }
+]);
